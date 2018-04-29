2b61beec284871701e33961d899cc76694bc2b24
EC2 Default User <ec2-user@ip-172-31-18-127.us-east-2.compute.internal>
2018-04-29 00:32:45 +0000

    added authentication to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
+   before_save { self.email = email.downcase if email.present? }
+   
    after_create :send_new_user_emails
  
+   validates :email, 
+                presence: true, 
+                uniqueness: { case_sensitive: false }, 
+                length: { minimum: 3, maximum: 254 }
+   validates :password, presence: true, length: {minimum: 6}, if: "password_digest.nil?"
+   validates :password, length: {minimum: 6}, allow_blank: true 
+   
    private
  
    def send_new_user_emails
