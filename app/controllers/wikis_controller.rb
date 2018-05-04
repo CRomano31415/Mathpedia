ba423281d78aed8d70b7fc11e531e423f288e732
Claudia <cromano31415@gmail.com>
2018-05-04 18:34:38 -0500

    finished crud

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -1,4 +1,5 @@
 class WikisController < ApplicationController
+
   def index
   	@wikis = Wiki.all 
   end
@@ -12,10 +13,12 @@ class WikisController < ApplicationController
   end
 
   def create
-  	@wiki = Wiki.new
-  	@wiki.title = params[:wiki][:title]
-  	@wiki.body = params[:wiki][:body]
-  	@wiki.private = params[:wiki][:private]
+  	@wiki = Wiki.new(wiki_params)
+  	# @wiki.title = params[:wiki][:title]
+  	# @wiki.body = params[:wiki][:body]
+  	# @wiki.private = params[:wiki][:private]
+    @wiki.user = current_user 
+
 
   	if @wiki.save
   		flash[:notice] = "Wiki was saved."
@@ -28,8 +31,10 @@ class WikisController < ApplicationController
 
   def update
   	@wiki = Wiki.find(params[:id])
-  	@wiki.title = params[:wiki][:title]
-  	@wiki.body = params[:wiki][:body]
+    @wiki.assign_attributes(wiki_params)
+
+  	# @wiki.title = params[:wiki][:title]
+  	# @wiki.body = params[:wiki][:body]
 
   	if @wiki.save
   		flash[:notice] = "Wiki was updated"
@@ -48,12 +53,17 @@ class WikisController < ApplicationController
   def destroy
   	@wiki = Wiki.find(params[:id])
 
-  	if @post.destroy
+  	if @wiki.destroy
   		flash[:notice] = "\"#{@wiki.title}\" was deleted successfully."
-  		redirect_to posts_path
+  		redirect_to wikis_path
   	else
   		flash.now[:alert] = "There was an error deleting the post."
   		render :show
   	end 
   end 
+
+def wiki_params
+  params.require(:wiki).permit(:title, :body, :user)
+end
+
 end
